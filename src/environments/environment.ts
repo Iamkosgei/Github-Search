c234b6fac167109d1ecefb33a400d8303aaa583c
iamkevin <kosgeik2930@gmail.com>
2019-03-31 18:27:17 +0300

    added api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: `d0bf12319a2fef42eb8ae5f321fa859cfe7ed2b3`
 };
 
 /*

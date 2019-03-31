7c0320dd6ae17d84cb7f86e4f63edfc22408fea7
iamkevin <kosgeik2930@gmail.com>
2019-03-31 18:17:46 +0300

    added a search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubSearchComponent } from './github-search/github-search.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubSearchComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
